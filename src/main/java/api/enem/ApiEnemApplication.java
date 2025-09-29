c08ac7422b59632952cd439cd01932b9d8d6396f
Isabel Henrique <isabel.henrique.dev@gmail.com>
2025-09-29 14:03:27 -0300

    first commit

---
+++ src/main/java/api/enem/ApiEnemApplication.java
@@ -0,0 +1,13 @@
+package api.enem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEnemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEnemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/api/enem/ApiEnemApplicationTests.java
@@ -0,0 +1,13 @@
+package api.enem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEnemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
